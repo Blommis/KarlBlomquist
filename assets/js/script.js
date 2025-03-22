7f25e0022f3fe0b52b4a5211185c6129170214d7
Blommis <blomquist799@gmail.com>
2025-03-22 13:30:03 +0100

    started to created my portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let jobTitles = ["Digital Marketing graduate", "Web Developer student", "Front End Developer student", "Back end student"];
+    let index = 0;
+    let output = document.querySelector(".typed-text-output");
+
+    function changeTitle() {
+        output.textContent = jobTitles[index];
+        index = (index + 1) % jobTitles.length;
+    }
+
+    changeTitle(); // Kör direkt första gången
+    setInterval(changeTitle, 2000); // Byt titel var 2:e sekund
+});
